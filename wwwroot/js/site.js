a91d4a00a19f9b18ecc97e56aa0bf0be8b2a5f4b
Erick Garcia <penado188@gmail.com>
2023-10-18 14:35:01 -0600

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const btnTheme = document.getElementById('btnTheme')
+const iconTheme = document.getElementById('iconTheme')
+
+document.addEventListener('DOMContentLoaded', () =>{
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+        iconTheme.classList.replace('fa-moon','fa-sun')
+
+    } else {
+        document.documentElement.classList.remove('dark');
+        iconTheme.classList.replace('fa-sun','fa-moon')
+    }
+});
+
+const handleTheme = () => {
+    if(document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark');
+        iconTheme.classList.replace('fa-sun','fa-moon')
+        localStorage.theme = "light";
+    }
+    else{
+        document.documentElement.classList.add('dark');
+        iconTheme.classList.replace('fa-moon','fa-sun')
+        localStorage.theme = "dark";
+    }
+}
+
+btnTheme.addEventListener('click', handleTheme);
