49ac07198ecc566a527ff67abfe6ec26b7280f38
Erick Garcia <penado188@gmail.com>
2024-04-21 19:06:32 -0600

    salon de evento y funcionalidades

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -27,4 +27,8 @@ public class LibraryController : Controller
     public IActionResult Menu(){
         return View();
     }
+
+    public IActionResult SalonEvento(){
+        return View();
+    }
 }
